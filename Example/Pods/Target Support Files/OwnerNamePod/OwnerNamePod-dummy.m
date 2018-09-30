d679ea39f661d91fc55e373858f3206e4e7a145f
rohitneo002 <rohitneo002@gmail.com>
2018-09-30 14:08:00 +0530

    initial commit

---
+++ Example/Pods/Target Support Files/OwnerNamePod/OwnerNamePod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OwnerNamePod : NSObject
+@end
+@implementation PodsDummy_OwnerNamePod
+@end
---
+++ Example/Pods/Target Support Files/Pods-OwnerNamePod_Tests/Pods-OwnerNamePod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OwnerNamePod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_OwnerNamePod_Tests
+@end
